c02f1b4f75a44416942f26d3e3e32f61d8e66123
jychang06 <jychang@wisc.edu>
2025-11-22 21:28:44 -0600

    ligma

--- .conda/trader_joes_parser.py
+++ .conda/trader_joes_parser.py
@@ -1,11 +1,15 @@
-from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-class TraderJoesParser:
-    def get_name():
+from .parser_base import ParserBase
+
+class TraderJoesParser(ParserBase):
+    def get_name(self, html):
 
     
-    def get_price():
+    def get_price(self, html):
 
 
         
