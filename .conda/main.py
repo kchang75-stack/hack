55b1f5c0bbc883e53334e90725f23a2c09357980
Keng <kuvyojkeng@hotmail.com>
2025-11-23 04:00:47 -0600

    first server commit

---
+++ .conda/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/find/<user_id>")
+def get_user(user_id):
+    data = {
+        "item": user_id,
+        "lowestPrice": "0",
+    }
+
+    return render_template("indextest.html")
+
+@app.route("/test")
+def test():
+    value = 4
+    return jsonify({"value": value})
+
+
+if __name__ == "__main__":
+    app.run(host="10.140.189.214",port=80, debug=True)
